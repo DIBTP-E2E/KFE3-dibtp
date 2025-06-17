381684a7231e901fde64aa17c34be3c99e79ea55
Jiiker <motive46@naver.com>
2025-06-17 23:32:42 +0900

    chore: prefix 종류 수정

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,23 +5,17 @@ module.exports = {
       2,
       'always',
       [
-        'feat', // 새로운 기능
+        'feat', // 새로운 기능 추가
         'fix', // 버그 수정
-        'docs', // 문서만 변경
-        'style', // 코드 포맷팅 등
-        'refactor', // 코드 리팩터링
-        'perf', // 성능 개선
-        'test', // 테스트 코드
-        'chore', // 기타 작업 (.gitignore 등)
-        'build', // 빌드 관련
-        'ci', // CI 관련
-        'revert', // 커밋 되돌리기
-        'design', // UI 디자인
-        'hotfix', // 긴급 수정
-        'comment', // 주석 추가/수정
+        'docs', // 문서 수정
+        'settings', // 프로젝트 세팅 관련
+        'refactor', // 코드 리팩토링
+        'test', // 테스트 코드 추가/리팩토링
+        'chore', // 패키지 매니저 등 기타 수정
+        'design', // 사용자 UI 디자인 변경
+        'comment', // 주석 추가 또는 수정
         'rename', // 파일/폴더명 변경
         'remove', // 파일 삭제
-        'settings', // 프로젝트 세팅 관련
       ],
     ],
     'subject-empty': [2, 'never'], // 제목 비어있으면 안됨
