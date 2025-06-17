77e570fbca0aa67f998b9e1867ef9c08aa134f84
Jiiker <motive46@naver.com>
2025-06-18 00:45:36 +0900

    settings: tanstack query 설치 및 provider 설정 (#1)

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Noto_Sans_KR } from 'next/font/google';
 import './globals.css';
+import QueryProvider from './providers/QueryProvider';
 
 const notoSansKR = Noto_Sans_KR({
   variable: '--font-noto-sans',
@@ -13,12 +14,12 @@ export const metadata: Metadata = {
   description: '독특하고 재미있는 경매 시스템을 통해 중고 물품을 거래할 수 있는 플랫폼입니다.',
 };
 
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={notoSansKR.variable}>{children}</body>
+    <html lang="ko">
+      <body className={notoSansKR.variable}>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
     </html>
   );
-};
-
-export default RootLayout;
+}
