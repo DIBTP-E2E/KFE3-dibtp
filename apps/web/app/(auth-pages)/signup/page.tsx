74905cd40475423bc0e825992b8d8c929caea58d
Jiiker <motive46@naver.com>
2025-06-21 20:35:29 +0900

    feat: 회원가입 페이지 구현 (#9)

--- apps/web/app/(auth-pages)/signup/page.tsx
+++ apps/web/app/(auth-pages)/signup/page.tsx
@@ -1,1 +1,28 @@
 // 회원가입 페이지
+import {
+  SignupTitle,
+  NameInput,
+  EmailInput,
+  PasswordInput,
+  SignupButton,
+  LoginInfo,
+} from '../../../components/signup';
+
+const SignupPage = () => {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <div className="w-full max-w-[375px] px-4">
+        <SignupTitle />
+        <div className="space-y-4 pt-4 pb-12">
+          <NameInput />
+          <EmailInput />
+          <PasswordInput />
+        </div>
+        <SignupButton />
+        <LoginInfo />
+      </div>
+    </main>
+  );
+};
+
+export default SignupPage;
