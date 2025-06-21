67037d3038c1c01e27c5da1426d39ab0e14526c8
Jiiker <motive46@naver.com>
2025-06-21 20:24:04 +0900

    feat: 로그인 페이지 구현 (#9)

--- apps/web/app/(auth-pages)/login/page.tsx
+++ apps/web/app/(auth-pages)/login/page.tsx
@@ -1,1 +1,25 @@
-// 로그인 페이지
+import {
+  LoginTitle,
+  EmailInput,
+  PasswordInput,
+  LoginButton,
+  SignupInfo,
+} from '../../../components/login';
+
+const LoginPage = () => {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <div className="w-full max-w-[375px] px-4">
+        <LoginTitle />
+        <div className="space-y-4 py-12">
+          <EmailInput />
+          <PasswordInput />
+        </div>
+        <LoginButton />
+        <SignupInfo />
+      </div>
+    </main>
+  );
+};
+
+export default LoginPage;
