7b70e8062e116c691f89e2d9078dff74e009ec60
Jiiker <motive46@naver.com>
2025-06-23 00:46:38 +0900

    feat: 세션 로그인 로직 구현 (#9)

--- apps/web/services/login/setServerSession.ts
+++ apps/web/services/login/setServerSession.ts
@@ -1,8 +1,8 @@
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+import { createServerActionClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 
 export async function setServerSession(email: string, password: string) {
-  const supabase = createRouteHandlerClient({ cookies });
+  const supabase = createServerActionClient({ cookies });
   const { error } = await supabase.auth.signInWithPassword({ email, password });
 
   if (error) throw error;
