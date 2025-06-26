102d544db907af488f3caec8dece9fdc1c0d9b38
gold <goldegg127@gmail.com>
2025-06-27 03:01:38 +0900

    refactor: 스토리에서 사용하는 공통 컴포넌트 분리 (#33)

---
+++ packages/ui/src/storybook-components/index.ts
@@ -0,0 +1,5 @@
+export { default as StoryPage } from './StoryPage';
+export { default as PageTitle } from './PageTitle';
+export { default as Section } from './Section';
+export { default as SectionTitle } from './SectionTitle';
+export { default as HowToUse } from './HowToUse';
