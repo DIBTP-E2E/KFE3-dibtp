9183dc1636a0e34cb0928c2ab3d3236d96e2aa4c
gold <goldegg127@gmail.com>
2025-06-27 04:04:51 +0900

    docs: 스토리북 디자인 토큰 문서에서 Semantic 과 Scale Color 분리 (#33)

--- packages/ui/src/storybook-components/index.ts
+++ packages/ui/src/storybook-components/index.ts
@@ -3,3 +3,6 @@ export { default as PageTitle } from './PageTitle';
 export { default as Section } from './Section';
 export { default as SectionTitle } from './SectionTitle';
 export { default as HowToUse } from './HowToUse';
+export { default as HowToUseClass } from './HowToUseClass';
+export { default as ColorList } from './ColorList';
+export { default as ColorPalette } from './ColorPalette';
