0e7954ac0a3482205eb35c915c312e98ed37f692
mgyang53 <mg960503@gmail.com>
2025-07-01 21:11:49 +0900

    chore: prisma db 스키마 최신화 (#48)

--- apps/web/prisma/migrations/migration.sql
+++ apps/web/prisma/migrations/migration.sql
@@ -12,7 +12,7 @@ CREATE TABLE "bids" (
 -- CreateTable
 CREATE TABLE "chat_messages" (
     "chat_message_id" BIGSERIAL NOT NULL,
-    "chat_room_id" BIGINT NOT NULL,
+    "chat_room_id" UUID NOT NULL,
     "message" TEXT NOT NULL,
     "is_read" BOOLEAN NOT NULL DEFAULT false,
     "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -25,10 +25,10 @@ CREATE TABLE "chat_messages" (
 CREATE TABLE "chat_rooms" (
     "chat_room_id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "product_id" BIGINT NOT NULL,
-    "seller_user_id" UUID NOT NULL DEFAULT gen_random_uuid(),
-    "buyer_user_id" UUID NOT NULL DEFAULT gen_random_uuid(),
-    "created_at" TIMESTAMPTZ(6) NOT NULL,
-    "updated_at" TIMESTAMPTZ(6),
+    "seller_user_id" UUID NOT NULL,
+    "buyer_user_id" UUID NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "chat_rooms_pkey" PRIMARY KEY ("chat_room_id")
 );
@@ -85,11 +85,17 @@ CREATE UNIQUE INDEX "bids_product_id_key" ON "bids"("product_id");
 -- CreateIndex
 CREATE UNIQUE INDEX "bids_bid_id_key" ON "bids"("bid_id");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "unique_product_users" ON "chat_rooms"("product_id", "seller_user_id", "buyer_user_id");
+
 -- AddForeignKey
 ALTER TABLE "bids" ADD CONSTRAINT "bids_bidder_user_id_fkey" FOREIGN KEY ("bidder_user_id") REFERENCES "users"("user_id") ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
 ALTER TABLE "bids" ADD CONSTRAINT "bids_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("product_id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "chat_messages" ADD CONSTRAINT "chat_messages_chat_room_id_fkey" FOREIGN KEY ("chat_room_id") REFERENCES "chat_rooms"("chat_room_id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "chat_messages" ADD CONSTRAINT "chat_messages_sender_user_id_fkey" FOREIGN KEY ("sender_user_id") REFERENCES "users"("user_id") ON DELETE NO ACTION ON UPDATE NO ACTION;
@@ -98,7 +104,7 @@ ALTER TABLE "chat_messages" ADD CONSTRAINT "chat_messages_sender_user_id_fkey" F
 ALTER TABLE "chat_rooms" ADD CONSTRAINT "chat_rooms_buyer_user_id_fkey" FOREIGN KEY ("buyer_user_id") REFERENCES "users"("user_id") ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE "chat_rooms" ADD CONSTRAINT "chat_rooms_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("product_id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "chat_rooms" ADD CONSTRAINT "chat_rooms_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("product_id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "chat_rooms" ADD CONSTRAINT "chat_rooms_seller_user_id_fkey" FOREIGN KEY ("seller_user_id") REFERENCES "users"("user_id") ON DELETE NO ACTION ON UPDATE NO ACTION;
