5cd6adffb1e0098318b02c24a3ad17bfbd4592c8
Jiiker <motive46@naver.com>
2025-07-03 20:41:53 +0900

    feat: 좋아요 버튼 구현 (#22)

---
+++ apps/web/components/product-detail/LikeButton.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useState } from 'react';
+
+import { Icon } from '@repo/ui/components/Icons';
+
+export const LikeButton = () => {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleClick = () => {
+    setIsLiked((prev) => !prev);
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      className="rounded-full p-1 flex items-center justify-center bg-white shadow-sm"
+    >
+      <Icon
+        name={isLiked ? 'HeartFill' : 'Heart'}
+        size="xs"
+        color={isLiked ? 'primary' : 'default'}
+      />
+    </button>
+  );
+};
