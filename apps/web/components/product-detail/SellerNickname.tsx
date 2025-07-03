60a460a5b00a32fae59634d43c4e9d5cb38ba23f
Jiiker <motive46@naver.com>
2025-07-04 02:24:54 +0900

    feat: 상품 상세 페이지 유저 정보, 채팅 버튼 구현 (#22)

---
+++ apps/web/components/product-detail/SellerNickname.tsx
@@ -0,0 +1,10 @@
+
+interface SellerNicknameProps {
+  nickname: string;
+}
+
+const SellerNickname = ({ nickname }: SellerNicknameProps) => {
+  return <span className="font-semibold text-gray-800">{nickname}</span>;
+};
+
+export default SellerNickname;
