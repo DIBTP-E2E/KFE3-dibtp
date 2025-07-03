a4d73f776eaffc6fc4783a352d7f6cd9a69b4add
Jiiker <motive46@naver.com>
2025-07-03 20:41:11 +0900

    feat: 뒤로가기 버튼 구현 (#22)

---
+++ apps/web/components/product-detail/BackButton.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { Icon } from '@repo/ui/components/Icons';
+import { useRouter } from 'next/navigation';
+
+export const BackButton = () => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('/products');
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      className="rounded-full p-1 flex items-center justify-center bg-white shadow-sm"
+    >
+      <Icon name="ArrowLeft" size="xs" />
+    </button>
+  );
+};
