c84da0dbcd5982f3b9b9afcc7ca4d5f26352ced1
Jiiker <motive46@naver.com>
2025-07-04 02:23:28 +0900

    design: 입찰 버튼 디자인 수정 (#22)

--- apps/web/components/product-detail/BidButton.tsx
+++ apps/web/components/product-detail/BidButton.tsx
@@ -6,7 +6,7 @@ const BidButton = ({}: BidButtonProps) => {
   return (
     <button
       // onClick={onClick} // 입찰 로직 구현 시 다시 추가
-      className="px-6 py-3 rounded-full bg-bg-primary text-text-inverse font-semibold"
+      className="px-6 py-2 rounded-full bg-bg-primary text-text-inverse text-lg font-semibold"
     >
       입찰하기
     </button>
