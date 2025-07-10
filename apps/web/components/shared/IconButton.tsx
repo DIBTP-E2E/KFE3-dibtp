2cc135a91b38988e8a3980d69928b91d12a42091
gold <goldegg127@gmail.com>
2025-07-11 05:29:35 +0900

    feat: web app shared 의 Icon 컴포넌트에 color, variant props 추가 (#123)

--- apps/web/components/shared/IconButton.tsx
+++ apps/web/components/shared/IconButton.tsx
@@ -5,12 +5,14 @@ import type { IconName } from '@repo/ui/components';
 import { cn } from '@repo/ui/utils/cn';
 
 interface IconButtonOwnProps<T extends ElementType = 'button'> {
-  iconName: IconName;
-  ariaLabel: string;
-
   // div, span은 react-router-dom의 <Link>와 함께 사용할 때 <a> 태그 중복 방지를 위해 사용.
   // Next.js의 <Link>는 자식이 <a> 태그일 경우 a 요소 중복이 자동 제거됨. <a> 태그 사용 권장. 불 필요한 div, span 사용 방지.
   as?: T;
+  iconName: IconName;
+  ariaLabel: string;
+  color?: 'dark' | 'light' | 'primary';
+  variant?: 'border' | 'bg';
+  className?: string;
 }
 
 type IconButtonProps<T extends ElementType = 'button'> = IconButtonOwnProps<T> &
@@ -18,10 +20,25 @@ type IconButtonProps<T extends ElementType = 'button'> = IconButtonOwnProps<T> &
 
 export type { IconButtonProps };
 
+const STYLE = {
+  border: {
+    light: 'border border-border-base text-text-base',
+    dark: 'border border-border-inverse text-text-inverse',
+    primary: 'border border-border-primary text-text-primary',
+  },
+  bg: {
+    light: 'bg-bg-base text-text-base',
+    dark: 'bg-bg-dark text-text-inverse',
+    primary: 'bg-bg-primary text-text-inverse',
+  },
+};
+
 const IconButton = <T extends ElementType = 'button'>({
   iconName,
   ariaLabel,
   as = 'button' as T,
+  color,
+  variant,
   className,
   ...restprops
 }: IconButtonProps<T>) => {
@@ -30,8 +47,9 @@ const IconButton = <T extends ElementType = 'button'>({
   return (
     <Component
       className={cn(
-        'inline-flex items-center justify-center w-[40px] h-[40px] text-text-base',
-        'border border-border-base rounded-full',
+        'inline-flex items-center justify-center w-[40px] h-[40px] rounded-full',
+        // STYLE[variant][color],
+        STYLE.bg.light,
         className
       )}
       aria-label={ariaLabel}
