1a7c1648952e0b43036688baa2b287a800985e5b
gold <goldegg127@gmail.com>
2025-07-14 15:49:37 +0900

    refactor: header icon 디렉토리 그룹화하여 아이콘버튼 컴포넌트 위치 이동 (#123)

---
+++ apps/web/components/layout/header-icon/index.ts
@@ -0,0 +1,4 @@
+export { default as AlarmButton } from './AlarmButton';
+export { default as BackButton } from './BackButton';
+export { default as SearchButton } from './SearchButton';
+export { default as RegionLabel } from './RegionLabel';
