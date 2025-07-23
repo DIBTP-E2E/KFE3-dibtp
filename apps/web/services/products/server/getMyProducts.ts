76fa4a49e777de8568c21005bce854a3b91b197c
mgyang53 <mg960503@gmail.com>
2025-07-23 18:43:27 +0900

    fix: @web 별칭 수정 (#188)

--- apps/web/services/products/server/getMyProducts.ts
+++ apps/web/services/products/server/getMyProducts.ts
@@ -1,4 +1,4 @@
-import { prisma } from '@/lib/prisma';
+import { prisma } from '@web/lib/prisma';
 
 import type { ProductsAPIResponse, ProductStatus } from '@web/types';
 import { getAuthenticatedUser } from '@web/utils/auth/server';
