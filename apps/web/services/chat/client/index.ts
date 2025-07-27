625ff815aceaf826b8e596d581fcc404b5597eef
gold <goldegg127@gmail.com>
2025-07-28 07:02:24 +0900

    feat: 채팅방 메시지 송수신하는 서비스함수와 api route 생성 (#202)

--- apps/web/services/chat/client/index.ts
+++ apps/web/services/chat/client/index.ts
@@ -1,3 +1,6 @@
 export * from './createChatRoom';
 export * from './fetchChatList';
 export * from './fetchChatRoomDetail';
+export * from './fetchMessages';
+export * from './sendMessage';
+export * from './markMessagesAsRead';
