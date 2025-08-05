274dff9f90ca76fa006d421035d9e427b87ce9b8
Jiiker <motive46@naver.com>
2025-08-05 15:28:06 +0900

    chore: 스타일 디자인 토큰값으로 변경

--- apps/web/components/product-detail/ProductAddress.tsx
+++ apps/web/components/product-detail/ProductAddress.tsx
@@ -5,9 +5,9 @@ interface ProductAddressProps {
 
 const ProductAddress = ({ region, detail_address }: ProductAddressProps) => {
   return (
-    <div className="mt-4">
-      <h2 className="text-lg font-bold text-gray-900 mb-2">거래 희망 장소</h2>
-      <p className="text-base text-gray-700 whitespace-pre-wrap">{`${region} ${detail_address}`}</p>
+    <div className="mt-md">
+      <h2 className="text-lg font-bold mb-sm">거래 희망 장소</h2>
+      <p className="text-base whitespace-pre-wrap">{`${region} ${detail_address}`}</p>
     </div>
   );
 };
